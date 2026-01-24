63d26d43009f368aa4c9fa8b51459d7df0a8c1cf
Smnthjm08 <sumanthjmm.04@gmail.com>
2026-01-24 23:01:15 +0530

    feat: migrated to turborepo

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main className="flex-col items-center min-h-screen justify-center">
+      <div className="font-black text-4xl">Work with us</div>
+      <div className="font-black">font black</div>
+      <div className="font-extrabold">font extrabold</div>
+      <div className="font-bold">font bold</div>
+      <div className="font-semibold">font semibold</div>
+      <div className="font-medium">font medium</div>
+      <div className="font-normal">font extralight</div>
+      <div className="font-light">font light</div>
+      <div className="font-extralight">font extralight</div>
+      <div className="font-thin">font thin</div>
+    </main>
+  );
+}
